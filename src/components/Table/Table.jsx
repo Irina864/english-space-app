ec7389ae7cde42f07f485475d3a27dea2460b309
Irina864 <irina.peslyak.1864@gmail.com>
2024-06-18 13:46:23 +0300

    add functionality

--- src/components/Table/Table.jsx
+++ src/components/Table/Table.jsx
@@ -2,51 +2,40 @@ import { useState } from 'react';
 import Topic from '../Topic/Topic';
 import Input from '../Input/Input';
 import './Table.css';
-import Slider from '../Slider/Slider';
+import { useNavigate } from 'react-router-dom';
 
 function Table(props) {
     const data = props.data;
-    const [selectedPageMode, setSelectedPageMode] = useState(null);
     const [clickedIndex, setClickedIndex] = useState(0);
-    // const navigate = useNavigate();
-    // const handleSelectedPageMode = () => {
-    //     const currentPath = window.location.pathname;
-    //     const newPath = `${currentPath}/cards`;
-    //     navigate(newPath);
-    // };
-    const handleSelectedPageMode = () => {
-        setSelectedPageMode(!selectedPageMode);
+    const navigate = useNavigate();
+    const handleClickedIndex = () => {
+        const newPath = `/cards`;
+        navigate(newPath);
     };
     let originalBoolean = false;
 
     return (
         <main className="main">
-            {selectedPageMode ? (
-                <section className="main__card">
-                    <Slider data={data} index={clickedIndex} />
-                </section>
-            ) : (
-                <section className="main__table">
-                    <Input />
-                    {data.map((i, index) => (
-                        <div onClick={handleSelectedPageMode}>
-                            <Topic
-                                key={i.id}
-                                index={index}
-                                english={i.english}
-                                transcription={i.transcription}
-                                russian={i.russian}
-                                tags={i.tags}
-                                tags_json={i.tags_json}
-                                boolean={originalBoolean}
-                                clickedCard={(clickedIndex) =>
-                                    setClickedIndex(clickedIndex)
-                                }
-                            />
-                        </div>
-                    ))}
-                </section>
-            )}
+            <section className="main__table">
+                <Input />
+                {data.map((i, index) => (
+                    <div onClick={handleClickedIndex}>
+                        <Topic
+                            key={i.id}
+                            index={index}
+                            english={i.english}
+                            transcription={i.transcription}
+                            russian={i.russian}
+                            tags={i.tags}
+                            tags_json={i.tags_json}
+                            boolean={originalBoolean}
+                            clickedCard={(clickedIndex) =>
+                                setClickedIndex(clickedIndex)
+                            }
+                        />
+                    </div>
+                ))}
+            </section>
         </main>
     );
 }
